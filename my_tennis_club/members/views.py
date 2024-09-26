e3fc352b661679a5320b636998840bb4cfe1f183
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-09-26 16:44:12 +0500

    first commit

---
+++ my_tennis_club/members/views.py
@@ -0,0 +1,48 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+def members(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context,request))
+
+def details(request, slug):
+  mymember = Member.objects.get(slug=slug)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
+
+
+# For Testing making a view
+# def testing(request):
+#   template = loader.get_template('template.html')
+#   context = {
+#     'fruits': ['Apple', 'Banana', 'Cherry'],   
+#   }
+#   return HttpResponse(template.render(context, request))
+
+# def testing(request):
+#   mymembers = Member.objects.all().values()
+#   template = loader.get_template('template.html')
+#   context = {
+#     'mymembers': mymembers,
+#   }
+#   return HttpResponse(template.render(context, request))
+
+def testing(request):
+  mydata = Member.objects.all().order_by('-firstname').values()
+  template = loader.get_template('template.html')
+  context = {
+    'mymembers': mydata,
+  }
+  return HttpResponse(template.render(context, request))
